707b854d48746a07a8faece719f046d44cd3afbb
forallx94 <forallx94@gmail.com>
2021-01-02 22:45:15 +0900

    Make one file - 고압가스안전관리법

--- 법령 정제/distinct_law.py
+++ 법령 정제/distinct_law.py
@@ -1,7 +1,7 @@
-from law_preprocessing import uncircle
-
-import re
+import win32com.client as win32
 import pandas as pd
+import re, os, glob
+
 
 def seperate_Content(my_df,DocContents,law_name):
     # 절을 구분하는 과정
@@ -212,4 +212,143 @@ def self_Content(line, clause_dict):
             temp += clause_dict[i]
         new_line = re.sub(clause_list[5],temp,line)        
         return new_line
+
+
+
+# doc를 텍스트화 하여 받는 코드
+def doc2txt(Data_path):
+    files = glob.glob(Data_path)
+
+    for fname in files:
+        word = win32.Dispatch("Word.Application")
+        # word.Visible = 0
+
+        doc1 = word.Documents.Open(fname)
+
+        DocContents = str(doc1.Content.Text)
+        DocContents = DocContents.replace('\r', '\n')
+        word.ActiveDocument.Close()
+        word.Quit()
+    return DocContents
+
+
+def erase_(docs):
+    re.compile(r'<.*?>')
+    docs = re.sub(r'<.*?>','',docs)
+    return docs
+
+
+# 유니코드를 이용한 ① 1로 변경하는 과정
+def uncircle(s):
+    for i in range(1, 21):
+        s = s.replace(chr(0x245f + i), str(i))
+    return s.replace('\u24ea', '0')
+
+
+def seperate_other_law(line, input_list = []):
+    temp = [x for x in re.finditer('「',line)]
+    if len(temp) == 0:
+        input_list += [line]
+        return input_list
+    if len(temp) == 1:
+        T = [line[:temp[0].span(0)[0]], line[temp[0].span(0)[0]:]]
+        while True:
+            try: T.remove('')
+            except: break
+        input_list += T
+        return input_list
+    line_num = temp[len(temp)//2].span(0)[0]
+    seperate_other_law(line[:line_num],input_list)
+    seperate_other_law(line[line_num:],input_list)
+    return input_list
+    
+
+a = re.compile('(제\d+조(의\d+)?)(제\d+항(의\d+)?)(제\d+호(의\d+)?)|'+\
+               '(제\d+조(의\d+)?)(제\d+호(의\d+)?)|'+
+               '(제\d+조(의\d+)?)(제\d+항(의\d+)?)|'+\
+               '(제\d+조(의\d+)?)|'+\
+               '(제\d+항(의\d+)?)(제\d+호(의\d+)?)|'+\
+               '(제\d+항(의\d+)?)|'+\
+               '(제\d+호(의\d+)?)')
+
+    
+def seperate_clause(line,input_list = []):
+    global a
+    temp = [x for x in re.finditer(a,line)]
+    if len(temp) == 0:
+        input_list += [line]
+        return input_list
+    if len(temp) in [1,2]:
+        T = [line[:temp[0].span(0)[1]], line[temp[0].span(0)[1]:]]
+        while True:
+            try: T.remove('')
+            except: break
+        input_list += T
+        return input_list
+    line_num = temp[len(temp)//2].span(0)[1]
+    seperate_clause(line[:line_num],input_list)
+    seperate_clause(line[line_num:],input_list)
+    return input_list
+
+if __name__ == "__main__":
+
+    Data_path = "./위험물안전관리법.doc"
+    law_name = '위험물안전관리법'
+
+    write_path = './법 정규편식'
+    os.makedirs(write_path, exist_ok=true)
+
+    # 꺽쇠 유지 변수, 0 이면 유지 1 이면 삭제
+    brackets_exsit = 1
+
+    DocContents = doc2txt(Data_path)
+    DocContents = erase_(DocContents)
+
+    my_df = pd.DataFrame(columns = ["index", "Content"])
+    my_df = seperate_Content(my_df,DocContents,law_name)
+    my_df = full_index(my_df,law_name)
+
+    # my_df의 Content 수정 하는 작업. 
+    Content_law = re.compile( r'「(.*?)」')
+
+    clause = re.compile('(제\d+조(의\d+)?)(제\d+항(의\d+)?)(제\d+호(의\d+)?)|'+\
+                '(제\d+조(의\d+)?)(제\d+호(의\d+)?)|'+
+                '(제\d+조(의\d+)?)(제\d+항(의\d+)?)|'+\
+                '(제\d+조(의\d+)?)|'+\
+                '(제\d+항(의\d+)?)(제\d+호(의\d+)?)|'+\
+                '(제\d+항(의\d+)?)|'+\
+                '(제\d+호(의\d+)?)')
+
+    for num, line in enumerate(my_df["Content"]):
+        # 다른 법이 나왔으나 해당 법의 구체적인 조항이 언급되었는지 언급이 없는지 구별해야
+        if re.search(r'\[.*?\]',line):
+            my_df.iloc[num]["Content"] = ''
+            continue
+        clause_dict = {'법':law_name,'조': '','항':'','호':'','목':''}
+        try:
+            clause_dict = set_dict(my_df.iloc[num]['index'], clause_dict)
+        except:
+            pass
+        lines = seperate_clause(line,[])
+        new_line =''
+        for i in lines:
+            if Content_law.search(i) and clause.search(i):
+                clause_dict['법'] = Content_law.search(i).group(brackets_exsit)
+                i = re.sub(Content_law,'',i)
+            if Content_law.search(i) and not clause.search(i):
+                i = re.sub(Content_law,'\g<%d>'%brackets_exsit,i)
+            new_line += self_Content(i, clause_dict)
+        my_df.iloc[num]["Content"] = new_line
+        
+        
+    my_df.to_excel(r'C:\Users\POP\Desktop\work\5-4\법 정규편식\example.xlsx')
+
+    f = open(os.path.join(write_path , 'example.txt'), 'w', encoding='UTF8')
+    for index, content in zip(my_df["index"],my_df["Content"]):
+        if index != None:
+            f.write(index)
+            f.write('   ')
+        f.write(content)
+        
+    f.close()
 
